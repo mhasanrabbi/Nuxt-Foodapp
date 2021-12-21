47097cd42f9ee3254c8db88d19751909354f2874
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-12-21 13:36:08 +0600

    grid from store data

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,17 @@
 <template>
   <main class="container">
     <AppHeader />
+    <AppRestaurantInfo />
   </main>
 </template>
 
 <script>
 import AppHeader from "@/components/AppHeader.vue";
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue";
 
 export default {
   components: {
-    AppHeader,
+    AppRestaurantInfo,
   },
 };
 </script>
